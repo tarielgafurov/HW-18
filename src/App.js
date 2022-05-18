2efbb90c11e57f472fe6d95066fd74dba0979696
tarielgafurov <tarielgafurov618@gmail.com>
2022-05-18 15:34:07 +0300

    hw-18

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { useEffect, useRef, useState } from 'react';
+import styles from './Colors.module.css'
+
+function App() {
+
+  const [time , setTime] = useState(false)
+  const container = useRef(null)
+
+  useEffect(() => {
+
+    setTimeout(() => {
+      setTime(!time)
+    } , 1000)
+
+  } , [time])
+  return <div ref={container}  className={!time ? styles.red : styles.green}></div>
+}
+
+export default App;
